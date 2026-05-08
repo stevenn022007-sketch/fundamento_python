35145df407d569caed55cdf53decfa97d95cc6fc
7Stvn0 <stevenn022007@gmail.com>
2026-05-08 12:21:15 -0500

    add.file.Ejercicios_practicos

---
+++ Ejercicios_practicos .py
@@ -0,0 +1,71 @@
+# from ast import match_case
+# import math
+# # Ejercicio 1
+# nombre = "Steven"
+# producto = 20000
+# promedio = 4.5
+# print(f"el alumno {nombre} tiene un producto de valor {producto} y un promedio de {promedio}")
+
+# # Ejercicio 2
+# numero1 = int(input("Ingrese un valor entero "))
+# numero2 = int(input("ingrese un valor entero "))
+# numero3 = float(input("ingrese valores decimales "))
+# numero4 = input("ingrese un numero ")
+# numero5 = input("ingrese el segundo numero ")
+
+# suma = numero1 + numero2 + numero3
+# print(f"la suma de los numeros es {suma}")
+
+# numero_mayor = max(numero1, numero2, numero3)
+# print(f"el numero mayor es {numero_mayor}")
+
+# division = numero3
+
+# division /= (numero1 % numero2)
+# print(division)
+
+# concatenacion = numero4 + numero5
+# print(concatenacion)  
+
+# #ejercicio 3
+# base = 5
+# exponente = 3
+# potenciacion = base ** exponente
+
+# #ejercicio 4
+# operacion_raiz = int(input("ingresa un numero entero "))
+# raiz = round(math.sqrt(operacion_raiz))
+# print(f"el resultado de la raiz es: {raiz}")
+
+# #ejercicio 5
+
+# estudiante = input("Ingrese el nombre del estudiante: ")
+# decimal1 = float(input("ingrese el valor: "))
+# decimal2 = float(input("ingrese el valor: "))
+# decimal3 = float(input("ingrese el valor: "))
+# decimal4 = float(input("ingrese el valor: "))
+# decimal5 = float(input("ingrese el valor: "))
+
+# promedio_estudiante = (decimal1 + decimal2 + decimal3 + decimal4 + decimal5) / 5
+# print(f"Resultado: {estudiante},{promedio_estudiante}")
+
+
+#ejercicio 6
+
+# numeroUno = 8
+# numeroDos = 2
+# auxiliar = numeroDos
+# numeroDos = numeroUno
+# print(auxiliar)
+# print(numeroDos)
+
+
+#ejercicio 7
+# estado = False
+# estado += (5 == 2) or (2 > 1)
+# print(estado)
+
+
+#ejercicio 8
+resultado = (9/2) + 8*2/1 -(2+2)
+print(resultado )
